e2eb097d586be5a2fbb9a151f903d9fc5d17fcfd
hidekitsuruta <hickyjackpot69@i.softbank.jp>
2025-04-16 23:54:33 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,10 @@
+// 今日の日付を取得
 const today = new Date();
-const formatted = today.toLocaleDateString("ja-JP", {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-});
-console.log(formatted);
+
+// 年、月、日を取得（※月は0から始まるため+1する）
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+// コンソールに出力
+console.log(`${year}年${month}月${day}日`);
