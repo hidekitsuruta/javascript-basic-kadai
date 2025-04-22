b49d0c77e5c592d27330a7ef7e4c34ff5d9d62e0
hidekitsuruta <hickyjackpot69@i.softbank.jp>
2025-04-22 18:19:11 +0900

    Add files via upload

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,6 +1,5 @@
-const number = (price) => {
-    console.log(price * 10 );
+const number = (num) => {
+    return num * num ;
   }
   
-  // 関数を呼び出し、引数として購入金額を渡す
-  number(10);
+  console.log(number(10));
